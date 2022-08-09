11972851d945a4159df1a51ed19c1f2016800688
BrianGossett <brian.gossett.usa@gmail.com>
2022-08-09 11:04:39 -0400

    can remove a fish

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { gameList } = require('./handlers/GameList');
 const { gamesFish } = require('./handlers/GamesFish');
 const { addFish } = require('./handlers/AddFish');
 const { addGame } = require('./handlers/AddGame');
+const { removeFish } = require('./handlers/RemoveFish');
 
 const PORT = process.env.PORT || 5050
 
@@ -24,6 +25,7 @@ app.post('/newfish', addFish);
 
 app.post('/newgame', addGame);
 
+app.post('/removefish', removeFish);
 
 app.listen(PORT, function () {
 console.log(`Demo project at: ${PORT}!`); });
